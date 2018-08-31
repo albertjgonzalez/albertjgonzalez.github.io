0395145f30e27363ead62bd4aef8fda35d999b07
albertg1114 <ajgonzalez1114@gmail.com>
2018-08-31 00:17:26 -0400

    created heading for index page

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 window.scroll(0, 200);
+var heading = document.getElementById('heading');
+var audio = document.getElementById('audio-logo');
+var about = document.getElementById('aboutme-logo');
+var coding = document.getElementById('coding-logo');
+var visual = document.getElementById('visual-logo');
 
 
 
-
-
-
-const audio = document.getElementById('audio-logo');
-
-
-  audio.addEventListener('click' , () => {
+audio.addEventListener('click' ,function () {
   alert("content coming soon");
   });
+
+
+  audio.addEventListener("mouseover",function(){
+    heading.innerHTML= 'Audio';
+  }); 
+  audio.addEventListener("mouseout",function(){
+    heading.innerHTML= '';
+  }); 
+  about.addEventListener("mouseover",function(){
+    heading.innerHTML= 'About Me';
+  }); 
+  about.addEventListener("mouseout",function(){
+    heading.innerHTML= '';
+  }); 
+  coding.addEventListener("mouseover",function(){
+    heading.innerHTML= 'Coding';
+  }); 
+  coding.addEventListener("mouseout",function(){
+    heading.innerHTML= '';
+  }); 
+  visual.addEventListener("mouseover",function(){
+    heading.innerHTML= 'Visual';
+  }); 
+  visual.addEventListener("mouseout",function(){
+    heading.innerHTML= '';
+  }); 
