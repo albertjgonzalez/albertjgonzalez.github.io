a16adfaa5a2bf342a26c8aaa80e39e652a536811
albertg1114 <ajgonzalez1114@gmail.com>
2018-09-04 10:37:48 -0400

    fixed home bug

--- rework/script.js
+++ rework/script.js
@@ -1,46 +1,38 @@
 
-  var heading = document.getElementById('heading');
-  var audio = document.getElementById('audio-logo');
-  var about = document.getElementById('aboutme-logo');
-  var coding = document.getElementById('coding-logo');
-  var visual = document.getElementById('visual-logo');
-  var visualContent = document.getElementById('visualContent');
-  
-  
-  
-  
-    $(visual).click(function(){
-      $('#visualContent').slideUp();
-      visualContent.style.display = 'block';
-  });
-  
-    audio.addEventListener("mouseenter",function(){
-      heading.style.visibility = 'visible';
-      heading.innerHTML= 'Audio';
-    }); 
-    audio.addEventListener("mouseleave",function(){
-      heading.style.visibility = 'hidden';
-    }); 
-    about.addEventListener("mouseenter",function(){
-      heading.style.visibility = 'visible';
-      heading.innerHTML= 'Contact';
-    }); 
-    about.addEventListener("mouseleave",function(){
-      heading.style.visibility = 'hidden';
-    }); 
-    coding.addEventListener("mouseenter",function(){
-      heading.style.visibility = 'visible';
-      heading.innerHTML= 'Coding';
-    }); 
-    coding.addEventListener("mouseleave",function(){
-      heading.style.visibility = 'hidden';
-    }); 
-    visual.addEventListener("mouseenter",function(){
-      heading.style.visibility = 'visible';
-      heading.innerHTML= 'Visual';
-    }); 
-    visual.addEventListener("mouseleave",function(){
-      heading.style.visibility = 'hidden';
-    }); 
-  
- 
+var heading = document.getElementById('heading');
+var audio = document.getElementById('audio-logo');
+var about = document.getElementById('aboutme-logo');
+var coding = document.getElementById('coding-logo');
+var visual = document.getElementById('visual-logo');
+
+
+
+
+  audio.addEventListener("mouseenter",function(){
+    heading.innerHTML= 'Audio';
+  }); 
+  audio.addEventListener("mouseleave",function(){
+    heading.innerHTML= '';
+  }); 
+  about.addEventListener("mouseenter",function(){
+    heading.innerHTML= 'About Me';
+  }); 
+  about.addEventListener("mouseleave",function(){
+    heading.innerHTML= '';
+  }); 
+  coding.addEventListener("mouseenter",function(){
+    heading.innerHTML= 'Coding';
+  }); 
+  coding.addEventListener("mouseleave",function(){
+    heading.innerHTML= '';
+  }); 
+  visual.addEventListener("mouseenter",function(){
+    heading.innerHTML= 'Visual';
+  }); 
+  visual.addEventListener("mousleave",function(){
+    heading.innerHTML= '';
+  }); 
+
+function newFunction() {
+  return "mouseout";
+}
--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 
-var heading = document.getElementById('heading');
-var audio = document.getElementById('audio-logo');
-var about = document.getElementById('aboutme-logo');
-var coding = document.getElementById('coding-logo');
-var visual = document.getElementById('visual-logo');
-
-
-
-audio.addEventListener('click' ,function () {
-  alert("content coming soon");
-  });
-
-
-  audio.addEventListener("mouseenter",function(){
-    heading.innerHTML= 'Audio';
-  }); 
-  audio.addEventListener("mouseleave",function(){
-    heading.innerHTML= '';
-  }); 
-  about.addEventListener("mouseenter",function(){
-    heading.innerHTML= 'About Me';
-  }); 
-  about.addEventListener("mouseleave",function(){
-    heading.innerHTML= '';
-  }); 
-  coding.addEventListener("mouseenter",function(){
-    heading.innerHTML= 'Coding';
-  }); 
-  coding.addEventListener("mouseleave",function(){
-    heading.innerHTML= '';
-  }); 
-  visual.addEventListener("mouseenter",function(){
-    heading.innerHTML= 'Visual';
-  }); 
-  visual.addEventListener("mousleave",function(){
-    heading.innerHTML= '';
-  }); 
-
-function newFunction() {
-  return "mouseout";
-}
+  var heading = document.getElementById('heading');
+  var audio = document.getElementById('audio-logo');
+  var about = document.getElementById('aboutme-logo');
+  var coding = document.getElementById('coding-logo');
+  var visual = document.getElementById('visual-logo');
+  
+  
+  
+  
+  
+    
+  
+    audio.addEventListener("mouseenter",function(){
+      heading.style.visibility = 'visible';
+      heading.innerHTML= 'Audio';
+    }); 
+    audio.addEventListener("mouseleave",function(){
+      heading.style.visibility = 'hidden';
+    }); 
+    about.addEventListener("mouseenter",function(){
+      heading.style.visibility = 'visible';
+      heading.innerHTML= 'Contact';
+    }); 
+    about.addEventListener("mouseleave",function(){
+      heading.style.visibility = 'hidden';
+    }); 
+    coding.addEventListener("mouseenter",function(){
+      heading.style.visibility = 'visible';
+      heading.innerHTML= 'Coding';
+    }); 
+    coding.addEventListener("mouseleave",function(){
+      heading.style.visibility = 'hidden';
+    }); 
+    visual.addEventListener("mouseenter",function(){
+      heading.style.visibility = 'visible';
+      heading.innerHTML= 'Visual';
+    }); 
+    visual.addEventListener("mouseleave",function(){
+      heading.style.visibility = 'hidden';
+    }); 
+  
+ 
